78e1b5c2f0a457234b449295c51c769d0fde5035
LOoLzeC <33969267+LOoLzeC@users.noreply.github.com>
2017-12-19 03:07:22 +0700

    Update DrXp.py

--- DrXp.py
+++ DrXp.py
@@ -29,10 +29,10 @@ time.sleep(1)
 os.system("cd banner;python2 banner1.py")
 print
 print
-print ""+N+"          =[ "+O+"DarkSploit v.1 by Deray"+N+"                               ]"
+print ""+N+"          =[ "+O+"DarkSploit v.1 by Deray"+N+"                                         ]"
 print "   + -- --=[ 8 Exploits - 10 Scanners 5 post - 17 virus                      ]"
-print "   + -- --=[ Blog "+O+"http://zumizec-com.waper.co/ "+N+"- Reports ig @yungreyyxrist   ]"
-print "   + -- --=[ free darksploit trial "+R+"->"+O+" http://zumizec-com.waper.co/ ]"
+print "   + -- --=[ Blog http://zumizec-com.waper.co/ "+N+"- Reports ig @yungreyyxrist   ]"
+print "   + -- --=[ free darksploit trial "+R+"->"+O+" http://zumizec-com.waper.co/           ]"
 print
 dr = raw_input(""+N+"DrXp > ")
 time.sleep(2)
