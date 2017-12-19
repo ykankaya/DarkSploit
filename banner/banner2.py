1026a5cfcd5af28632c344e5fd3fde1cfce4b503
LOoLzeC <33969267+LOoLzeC@users.noreply.github.com>
2017-12-19 16:36:00 +0700

    Create banner2.py

---
+++ banner/banner2.py
@@ -0,0 +1,19 @@
+N = '\033[1;37m' # White
+G = '\033[32m' # Green
+print ""+N+""
+print
+print " _______"
+print "<"+G+" uh oh"+N+" >"
+print " -------"
+print "  \ "
+print "   \                 __"
+print "    \               (OO)"
+print "     \              (  )"
+print "                    /--\ "
+print "       __          / \  \ "
+print "      UooU\.'@@@@@@'  \  )"
+print "      \__/(@@@@@@@@@@@) /"
+print "           (@@@@@@@@@)(("
+print "           'YY----YY'  \\"
+print "            ||    ||    >>"
+
