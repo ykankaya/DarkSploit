9c75d08c44a61adf95d6e7584a17cd8a920740d5
LOoLzeC <33969267+LOoLzeC@users.noreply.github.com>
2017-12-22 05:27:26 +0700

    Update DrXp.py

--- DrXp.py
+++ DrXp.py
@@ -10,9 +10,9 @@ N = '\033[1;37m' # White
 G = '\033[32m' # Green
 O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
-os.system("cd banner;python2 banner.py")
+os.system("cd banner;python2 banner1.py")
 print ""+N+"       =[ "+O+"DarkSploit v.2 by Deray"+N+"                                         ]"
-print "+ -- --=[ 13 Exploits - 10 Scanners 5 post - 25 virus                     ]"
+print "+ -- --=[ 13 Exploits - 10 Scanners 13 post - 25 virus                    ]"
 print "+ -- --=[ Blog http://zumizec-com.waper.co/ "+N+"- Reports ig @yungreyyxrist   ]"
 print "+ -- --=[ free darksploit trial  http://zumizec-com.waper.co/             ]"
 print
