3a46386f3113f399d92e5775851a7c0169e4b9e4
LOoLzeC <33969267+LOoLzeC@users.noreply.github.com>
2017-12-22 23:00:05 +0700

    Create banner4.py

---
+++ banner/banner4.py
@@ -0,0 +1,10 @@
+# Set Color
+L = '\033[1;36m' #L
+print ""+L+""
+print " ____________"
+print "< darksploit >"
+print " ------------"
+print "       \   ,__,"
+print "        \  (oo)____"
+print "           (__)    )\ "
+print "              ||--|| *"
